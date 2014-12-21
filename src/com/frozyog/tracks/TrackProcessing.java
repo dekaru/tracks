4629c9b4c5b23f48ae3fdba911a91409d79e54dd
David Álvarez <david@frozyog.com>
2014-12-20 22:36:14 -0600

    Segment identification inside a track

--- src/com/frozyog/tracks/TrackProcessing.java
+++ src/com/frozyog/tracks/TrackProcessing.java
@@ -3,6 +3,7 @@ package com.frozyog.tracks;
 import javafx.util.Pair;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class TrackProcessing {
@@ -16,8 +17,89 @@ public class TrackProcessing {
             System.out.println("[" + climbs.get(i).getKey() + ", " + climbs.get(i).getValue() + "]");
         }
 
-        // identify segments
+        // identify segments test 1
+        ArrayList<Pair<Double, Double>> segment = new ArrayList<Pair<Double, Double>>(Arrays.asList(
+                new Pair<Double, Double>(1.0, 2.0),
+                new Pair<Double, Double>(1.5, 2.3),
+                new Pair<Double, Double>(2.0, 1.8)
+            ));
+        ArrayList<Pair<Double, Double>> track   = new ArrayList<Pair<Double, Double>>(Arrays.asList(
+                new Pair<Double, Double>(3.7, 5.2),
+                new Pair<Double, Double>(1.0, 2.0),
+                new Pair<Double, Double>(1.5, 2.3),
+                new Pair<Double, Double>(8.0, 7.8),
+                new Pair<Double, Double>(8.2, 8.8),
+                new Pair<Double, Double>(9.1, 4.1),
+                new Pair<Double, Double>(1.0, 2.0),
+                new Pair<Double, Double>(1.5, 2.3),
+                new Pair<Double, Double>(2.0, 1.8),
+                new Pair<Double, Double>(5.5, 9.3)
+            ));
+        System.out.println(identifySegmentInTrack(track, segment));
+    }
 
+
+
+    /**
+     * Determine if a segment is considered inside a track. A segment is an ordered subset of points in a track.
+     * Identified segment points must all be included in order in the track for this method to return true.
+     *
+     * Example
+     *  input:
+     *      segment: (1.0, 2.0), (1.5, 2.3), (2.0, 1.8)
+     *      track:   (3.7, 5.2), (1.0, 2.0), (1.5, 2.3), (8.0, 7.8), (8.2, 8.8), (9.1, 4.1), (1.0, 2.0), (1.5, 2.3), (2.0, 1.8), (5.5, 9.3)
+     *  output:
+     *      true
+     *
+     * @param track   an ArrayList of Pair<Double, Double> which represent gps points that form a track (latitude and longitude)
+     * @param segment an ArrayList of Pair<Double, Double> which represent gps points that form a segment (latitude and longitude)
+     * @return A boolean value determining if the provided segment is included in the provided track.
+     */
+    public static boolean identifySegmentInTrack(ArrayList<Pair<Double, Double>> track, ArrayList<Pair<Double, Double>> segment) {
+        System.out.println("\nIdentifying segments ----> ");
+
+        int segmentIterator = -1;
+
+        // traverse our track
+        for (int i = 0; i < track.size(); i++) {
+            // get our current track's trackpoint
+            Pair<Double, Double> trackpoint = track.get(i);
+            System.out.print("trackpoint: " + trackpoint.getKey() + ", " + trackpoint.getValue());
+
+            // compare to our segment
+            if (segmentIterator != -1) {
+                // we were traversing a matching segment... are we still doing that?
+                if (trackpoint.equals(segment.get(segmentIterator))) {
+                    // has the segment finished?
+                    if ((segmentIterator + 1) == segment.size()) {
+                        // ... yes it has
+                        System.out.println("... segment has been matched!!!");
+                        return true;
+                    }
+
+                    // update segment iterator
+                    segmentIterator++;
+                } else {
+                    // show's over, guys, reset our segment iterator
+                    System.out.print("... segment has been cut off");
+                    segmentIterator = -1;
+                }
+            } else {
+                // maybe this time we'll get lucky
+                if (trackpoint.equals(segment.get(0))) {
+                    // print out a result
+                    System.out.print("... segment started");
+
+                    // we have started matching our segment... initialize segment iterator for the next cycle
+                    segmentIterator = 1;
+                }
+            }
+
+            // insert new line
+            System.out.println();
+        }
+
+        return false;
     }
 
 
@@ -32,6 +114,7 @@ public class TrackProcessing {
      * in the provided elevation profile.
      */
     public static ArrayList<Pair<Integer, Integer>> identifyClimbs(float[] elevations) {
+        System.out.println("\nIdentifying elevations ----> ");
 
         ArrayList<Pair<Integer, Integer>> climbs = new ArrayList<Pair<Integer, Integer>>();
 
